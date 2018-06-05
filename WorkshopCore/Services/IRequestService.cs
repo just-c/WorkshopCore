af28d38168db18b020f8b96d8fb9262fc0bd3362
Justin Ward <wjustin1994@gmail.com>
2018-06-04 20:06:57 -0400

    implemented RequestServices

---
+++ WorkshopCore/Services/IRequestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WorkshopCore.Models;
+
+namespace WorkshopCore.Services
+{
+    public interface IRequestService
+    {
+        Task<Request> Create(Request request);
+
+        void AddModelState(ModelStateDictionary modelState);
+        ModelStateDictionary ModelState { get; }
+
+    }
+}
---
+++ WorkshopCore/Services/RequestService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WorkshopCore.Models;
+
+namespace WorkshopCore.Services
+{
+    public class RequestService : IRequestService
+    {
+        private WorkshopContext _context;
+        private ModelStateDictionary _modelState;
+
+        public ModelStateDictionary ModelState
+        {
+            get
+            {
+                return _modelState;
+            }
+        }
+
+        public RequestService(WorkshopContext context)
+        {
+            _context = context;
+        }
+
+        public void AddModelState(ModelStateDictionary modelState)
+        {
+            _modelState = modelState;
+        }
+
+        /// <summary>
+        /// Creates a new request
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<Request> Create(Request request)
+        {
+            _context.Add(request);
+
+            await _context.SaveChangesAsync();
+
+            return request;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private bool Validate(Request request)
+        {
+            //TODO Implement validate method
+            return _modelState.IsValid;
+        }
+    }
+}
